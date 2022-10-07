bd7cab92ac029efea59a9ec536d01be55ae1fae5
hiredtocode <jasonhan@kakao.com>
2022-10-07 23:17:06 +0900

    Recursive

--- factorial.js
+++ factorial.js
@@ -1,3 +1,6 @@
+// This is an algorithm that's commonly used to run code without knowing the limit of depth.
+// Recursion needs to stopping point so make sure it's there so that it doesn't run endlessly.
+
 function factorial(n) {
 	if (n == 1 || n == 0) return 1;
 	else return n * factorial(n - 1);
